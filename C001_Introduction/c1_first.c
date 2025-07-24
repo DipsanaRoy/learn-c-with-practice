45d1a49c55ea73ec960b3ee38b42e1220eaf4bd6
Dipsana Roy <dipsanaroy@gmail.com>
2025-07-24 10:59:35 +0000

    Delete C001_Introduction directory

--- C001_Introduction/c1_first.c
+++
@@ -1,9 +0,0 @@
-// C1. My first C program.
-// Comment: ignored by the compiler, just here for us humans.
-
-#include<stdio.h>
-
-int main() {
-    printf("Hello I'm learning C with Harry");
-    return 0; // returns 0 after successful execution of main() function
-}
