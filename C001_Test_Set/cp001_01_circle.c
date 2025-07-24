60b12516bccf88e1c38b1466b142bb8a821d9a04
Dipsana Roy <dipsanaroy@gmail.com>
2025-07-24 13:41:15 +0000

    Add C001_Test_Set: Questions & Solutions

---
+++ C001_Test_Set/cp001_01_circle.c
@@ -0,0 +1,21 @@
+// CP1. Calculate area of circle and cylinder
+
+#include <stdio.h>
+#include <math.h> // M_PI --> to get accurate PI value
+
+int main()
+{
+    float radius, height;
+    printf("\nEnter the radius of a circle: ");
+    scanf("%f", &radius);
+
+    printf("\nArea of the Circle is %f\n", M_PI * radius * radius);
+
+    printf("\nEnter the radius of a cylinder: ");
+    scanf("%f", &radius);
+    printf("Enter the height of a cylinder: ");
+    scanf("%f", &height);
+
+    printf("\nArea of the cylinder is %f\n", 2 * M_PI * radius * (radius + height));
+    return 0;
+}
---
+++ C001_Test_Set/cp001_03_rectangle.c
@@ -0,0 +1,10 @@
+// CP1.3. Calculate Area of rectangle
+
+#include <stdio.h>
+
+int main()
+{
+    int length = 9, breadth = 4;
+    printf("Area of the rectangle is %d", length * breadth);
+    return 0;
+}
