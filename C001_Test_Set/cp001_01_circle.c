b74ad5b621ee50a33a3079d1f66e8d57195562c9
Dipsana Roy <dipsanaroy@gmail.com>
2025-08-04 20:38:33 +0000

    Chapter 1 Test Set Update
    ✍️ Comment improvements: cp001_01_circle.c

--- C001_Test_Set/cp001_01_circle.c
+++ C001_Test_Set/cp001_01_circle.c
@@ -1,7 +1,7 @@
 // CP1.1. Calculate area of circle and cylinder
 
 #include <stdio.h>
-#include <math.h> // M_PI --> to get accurate PI value
+#include <math.h> // M_PI: to get accurate PI value
 
 int main()
 {
